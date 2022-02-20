422a5456150863230176b4da8a6e3e5602000856
IrinaPerezhegina <94002224+IrinaPerezhegina@users.noreply.github.com>
2022-02-20 17:42:34 +0500

    addaptiv

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
 },
     plugins: [
         new MiniCssExtractPlugin({
-        filename: '[name].[contenthash].scss'
+        filename: '[name].[contenthash].css'
         }),
         new HtmlWebpackPlugin({
         filename: "index.html",
